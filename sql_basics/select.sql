583e4b1cfe2345fcf5ad3ff1fbf0a40e9e6a52f7
nastytsa <70845525+nastytsa@users.noreply.github.com>
2020-11-13 14:20:55 +0200

    Update select.sql

--- sql_basics/select.sql
+++ sql_basics/select.sql
@@ -1,7 +1,7 @@
 SELECT person.first_name, person.last_name, 
 	COUNT(DISTINCT order_item.order_id) AS total_orders,
 	SUM(order_item.quantity) AS total_items_bought, 
-	SUM(order_item.quantity * (item.price - order_item.discount)) AS 		total_money_spent 
+	SUM(order_item.quantity * (item.price - order_item.discount)) AS total_money_spent 
 FROM `person`
 LEFT JOIN `order` ON person.id = order.person_id
 LEFT JOIN `order_item` ON order.id = order_item.order_id
